172f8cb36e49a9a37e3df2e8faee450e86134161
NewtonCosta <newtonjgcosta@gmail.com>
2026-02-19 12:47:44 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+module "s3" {
+  source       = "./modules/s3"
+  project_name = var.project_name
+}
+
+module "sqs" {
+  source       = "./modules/sqs"
+  project_name = var.project_name
+}
+
+module "mediaconvert" {
+  source       = "./modules/mediaconvert"
+  project_name = var.project_name
+  bucket_name  = module.s3.bucket_name
+}
+
+module "lambda" {
+  source                = "./modules/lambda"
+  project_name          = var.project_name
+  bucket_name           = module.s3.bucket_name
+  queue_arn             = module.sqs.queue_arn
+  queue_url             = module.sqs.queue_url
+  mediaconvert_role_arn = module.mediaconvert.role_arn
+  job_template_name     = module.mediaconvert.job_template_name
+}
+
+module "eventbridge" {
+  source       = "./modules/eventbridge"
+  project_name = var.project_name
+  bucket_name  = module.s3.bucket_name
+  queue_arn    = module.sqs.queue_arn
+}
